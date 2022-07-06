a76447e80581c58fbbb21d33187ad2d86add9b09
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-07-06 15:31:14 -0300

    feat(Req01)
    implementado a interface Model req01

---
+++ src/interfaces/ModelInterface.ts
@@ -0,0 +1,7 @@
+export interface Model<T> {
+  create(body: T): Promise<T>,
+  read(): Promise<T[]>,
+  readOne(id: string): Promise<T | null>,
+  update(id: string, body: T): Promise<T | null>,
+  delete(id: string): Promise<T | null>,
+}
