570e25dbacf00661f6cf80bff50e9c1417be22c5
Rafael (Carvalho) Santos <rafaelalonsobarbosa@gmail.com>
2022-07-07 17:27:40 -0300

    feat(Req04: Model)
    implementada as Models Generica e Car

---
+++ src/models/carModel.ts
@@ -0,0 +1,10 @@
+// import { Model } from '../interfaces/ModelInterface';
+import MongoModel from '.';
+import { Car } from '../interfaces/CarInterface';
+import carSchemaModel from './schemas/carSchema';
+
+export default class CarModel extends MongoModel<Car> {
+  constructor(model = carSchemaModel) {
+    super(model);
+  }
+}
---
+++ src/models/schemas/carSchema.ts
@@ -0,0 +1,36 @@
+import { Schema, model } from 'mongoose';
+import { Car } from '../../interfaces/CarInterface';
+
+const carSchema = new Schema<Car>({
+  model: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: Boolean,
+  },
+  buyValue: {
+    type: Number,
+    required: true,
+  },
+  doorsQty: {
+    type: Number,
+    required: true,
+  },
+  seatsQty: {
+    type: Number,
+    required: true,
+  },
+});
+
+const carSchemaModel = model<Car>('Car', carSchema);
+
+export default carSchemaModel;
